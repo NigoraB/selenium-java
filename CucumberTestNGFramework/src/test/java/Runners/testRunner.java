6ec8f86ec1440d7ff2a7efe0e2df87ca65063667
Tony Reyes <42112790+thonzReyes@users.noreply.github.com>
2023-06-20 20:08:37 -0400

    delete unwanted folder

--- CucumberTestNGFramework/src/test/java/Runners/testRunner.java
+++
@@ -1,9 +0,0 @@
-package Runners;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-public class testRunner extends AbstractTestNGCucumberTests {
-
-
-}
